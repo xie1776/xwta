d3b2c7b25d5c132c4f7c33faf5ed5f0f09dc39a1
zhibin <zhibn>
2018-08-21 17:37:41 +0800

    连接修改

--- App/Lib/Action/Api/WechatAction.class.php
+++ App/Lib/Action/Api/WechatAction.class.php
@@ -187,7 +187,7 @@
                     'type' => JokeModel::TYPE_IMG,
                     );
                 $joke = D('Joke')->where($where)->order('rand()')->find();
-                $result_img = sprintf($imgTpl, $fromUsername, $toUsername, $time, $joke['title'], $joke['title'], $joke['img'], 'http://dwto.99meiti.com/Joke/detail/id/'.$joke['id']);
+                $result_img = sprintf($imgTpl, $fromUsername, $toUsername, $time, $joke['title'], $joke['title'], $joke['img'], 'http://www.99meiti.com/Joke/detail/id/'.$joke['id']);
                 echo $result_img;exit;
             }
             //处理评论
@@ -208,7 +208,7 @@
             else if((strpos($keyword,'美女')!==false) || $keyword==2)
             {
                 $looker = D('Looker')->order('rand()')->find();
-                $result_img = sprintf($imgTpl, $fromUsername, $toUsername, $time, $looker['title'], $looker['title'], $looker['picUrl'], 'http://dwto.99meiti.com/Joke/lookerDetail/id/'.$looker['id']);
+                $result_img = sprintf($imgTpl, $fromUsername, $toUsername, $time, $looker['title'], $looker['title'], $looker['picUrl'], 'http://www.99meiti.com/Joke/lookerDetail/id/'.$looker['id']);
                 //$result_img = sprintf($only_imgTpl, $fromUsername, $toUsername, $time, 'http://m.laijiemi.cn/index.php/Looker/detail/id/'.$looker['id'],'1234567890123456');
                 echo $result_img;die;
             }
