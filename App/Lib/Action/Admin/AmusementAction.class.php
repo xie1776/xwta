15ac7bb7022f254b0dff462a7d378a8d8b89a62b
zhibin <xie1776@126.com>
2018-09-01 09:41:28 +0800

    bug修复

--- App/Lib/Action/Admin/AmusementAction.class.php
+++ App/Lib/Action/Admin/AmusementAction.class.php
@@ -112,7 +112,27 @@
 			}
 		}
 
+		/**
+		 * 删除功能
+		 * @Author   zhibin
+		 * @DateTime 2018-09-01
+		 * @return   [type]     [description]
+		 */
+		public function deljoke()
+		{
+			$id = I('get.id', 0, 'intval');
+			$info = D('Joke')->where(array('id'=>$id))->find();
+			if(!$info){
+				$this->error('数据未发现');die;
+			}
 
+			$res = D('Joke')->where(array('id'=>$id))->delete();
+			if ($res) {
+				$this->success('删除成功');
+			} else {
+				$this->success('删除失败');
+			}
+		}
 		
 
 	}
